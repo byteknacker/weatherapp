43825c1bcc25e5bb204b54d9bb3ed6d5a11f4002
Xiangyu John Wu <xiangyu.wu.10@gmail.com>
2016-07-10 13:38:03 -0700

    Change content text for infowindow

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ function initMap() {
             };
 
             infoWindow.setPosition(pos);
-            // infoWindow.setContent('Location found.');
+            infoWindow.setContent("You're Here");
             map.setCenter(pos);
         }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
