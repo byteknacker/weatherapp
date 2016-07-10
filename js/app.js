5525dd50b8d3f773c9c5b5db85fed186dbffe0e6
Xiangyu John Wu <xiangyu.wu.10@gmail.com>
2016-07-10 13:32:01 -0700

    Complete refactoring of code
    This is taken one to one from
    https://developers.google.com/maps/documentation/javascript/examples/map-geolocation
    
    It should work now without any delay problems

--- js/app.js
+++ js/app.js
@@ -1,35 +1,40 @@
 "use strict";
 
-var userLocation = {};
-var map;
-
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: userLocation.latitude,
-            lng: userLocation.longitude
+            lat: -34.397,
+            lng: 150.644
         },
-        zoom: 8
+        zoom: 6
     });
+    var infoWindow = new google.maps.InfoWindow({
+        map: map
+    });
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
-function successCallback(position) {
-    // Gather all location data into the userLocation object
-    userLocation.latitude = position.coords.latitude;
-    userLocation.longitude = position.coords.longitude;
-    console.log(userLocation);
-}
-
-function errorCallback(error) {
-    // Display when there is location tracking enabled on device
-    // but there is still an error retrieving the location data
-    var errorMessage = "There was an error in getting your location.\n";
-    errorMessage += error;
-    console.log(errorMessage);
-}
-
-if (window.navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-} else {
-    console.log('Your browser does not natively support geolocation.');
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
 }
