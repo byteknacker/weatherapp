37acf972f944d4dffccf1d884704af3477f75745
Xiangyu John Wu <xiangyu.wu.10@gmail.com>
2016-07-10 12:26:43 -0700

    Remove jQuery document ready function
    According to: http://stackoverflow.com/questions/32040213/moving-google-maps-api-code-to-separate-file-jquery
    I need to put the whole code outside the document ready function of jquery.
    
    If this fails, I will try async and defer tags on every script at the bottom

--- js/app.js
+++ js/app.js
@@ -1,38 +1,38 @@
 "use strict";
 
-$(document)
-    .ready(function () {
-        var options = {
-            enableHighAccuracy: true
-        };
-        var userLocation = {};
-        var map;
+var options = {
+    enableHighAccuracy: true
+};
+var userLocation = {};
+var map;
 
-        function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: userLocation.latitude, lng: userLocation.longitude},
-                zoom: 8
-            });
-        }
-
-        function successCallback(position) {
-            // Gather all location data into the userLocation object
-            userLocation.latitude = position.coords.latitude;
-            userLocation.longitude = position.coords.longitude;
-            console.log(userLocation);
-        }
-
-        function errorCallback(error) {
-            // Display when there is location tracking enabled on device
-            // but there is still an error retrieving the location data
-            var errorMessage = "There was an error in getting your location.\n";
-            errorMessage += error;
-            console.log(errorMessage);
-        }
-
-        if (window.navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
-        } else {
-            console.log('Your browser does not natively support geolocation.');
-        }
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: userLocation.latitude,
+            lng: userLocation.longitude
+        },
+        zoom: 8
     });
+}
+
+function successCallback(position) {
+    // Gather all location data into the userLocation object
+    userLocation.latitude = position.coords.latitude;
+    userLocation.longitude = position.coords.longitude;
+    console.log(userLocation);
+}
+
+function errorCallback(error) {
+    // Display when there is location tracking enabled on device
+    // but there is still an error retrieving the location data
+    var errorMessage = "There was an error in getting your location.\n";
+    errorMessage += error;
+    console.log(errorMessage);
+}
+
+if (window.navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+} else {
+    console.log('Your browser does not natively support geolocation.');
+}
