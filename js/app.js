c00830728dcdb2b8b26ccced28f29b5c44789796
Xiangyu John Wu <xiangyu.wu.10@gmail.com>
2016-07-17 09:44:46 -0700

    Change selector to bootstrap-switch-container

--- js/app.js
+++ js/app.js
@@ -77,7 +77,7 @@ function initMap() {
                             app.units_tracker = "celsius";
                             $("#temp_degree")
                                 .append(app.celsius);
-                            $("#temp_units")
+                            $(".bootstrap-switch-container")
                                 .on("click", function () {
                                     if (app.units_tracker === "celsius") {
                                         $("#temp_degree")
