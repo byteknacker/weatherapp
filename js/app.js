66fc10d56b7483b07e25dfed7742c508cdd35cd9
Xiangyu John Wu <xiangyu.wu.10@gmail.com>
2016-07-17 09:47:48 -0700

    Try out temp_degree to select

--- js/app.js
+++ js/app.js
@@ -77,7 +77,7 @@ function initMap() {
                             app.units_tracker = "celsius";
                             $("#temp_degree")
                                 .append(app.celsius);
-                            $(".bootstrap-switch")
+                            $("#temp_degree")
                                 .on("click", function () {
                                     if (app.units_tracker === "celsius") {
                                         $("#temp_degree")
